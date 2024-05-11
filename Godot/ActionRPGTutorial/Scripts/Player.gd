98bb7895f585e581a32960d586aaf1dda1bfd444
Eric Herdian <eric.herdian@binus.ac.id>
2024-05-11 22:12:48 +0700

    5/11/2024

--- Godot/ActionRPGTutorial/Scripts/Player.gd
+++ Godot/ActionRPGTutorial/Scripts/Player.gd
@@ -4,10 +4,33 @@ const ACCELERATION = 500
 const FRICTION = 500
 const MAX_SPEED = 80
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
+@onready var animationPlayer = $AnimationPlayer
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+			#roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
+	# Player Movement
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -16,6 +39,7 @@ func _physics_process(delta):
 	if input_vector!= Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -23,3 +47,17 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	#elif Input.is_action_just_pressed()
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
+
+func roll_state(delta):
+	pass
